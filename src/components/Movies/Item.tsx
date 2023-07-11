760cbe6ec4bc1fb0346f653cfaf5bbddbfa9fbb3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-11 13:01:19 +0200

    refactor: use TMDB directly - for demo purposes - temporarily

--- src/components/Movies/Item.tsx
+++ src/components/Movies/Item.tsx
@@ -1,26 +1,35 @@
 import { format } from 'date-fns';
 
-import { API_ENDPOINT } from '@/config';
+// import { API_ENDPOINT } from '@/config';
 
 type Props = {
   data: Movie;
 };
 
-const MoviesItem = ({ data }: Props) => (
-  <li className="bg-gray-50 p-2 rounded-lg">
-    <img
-      className="aspect-[2/3] w-full rounded-md"
-      src={API_ENDPOINT + data.poster}
-      alt={data.title}
-      loading="lazy"
-    />
-    <div className="m-1 mt-2">
-      <h2 className="text-lg font-semibold truncate">{data.title}</h2>
-      <p className="text-sm text-gray-500">
-        {format(new Date(data.releaseDate), 'LLLL do, yyyy')}
-      </p>
-    </div>
-  </li>
-);
+const MoviesItem = ({ data }: Props) => {
+  // const imageUrl = API_ENDPOINT + data.poster;
+  // temporary because we don't want to see those request come in for a demo
+  // normally we can use above; API_ENDPOINT + data.poster
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${data.poster
+    .split('/')
+    .pop()}`;
+
+  return (
+    <li className="bg-gray-50 p-2 rounded-lg">
+      <img
+        className="aspect-[2/3] w-full rounded-md"
+        src={imageUrl}
+        alt={data.title}
+        loading="lazy"
+      />
+      <div className="m-1 mt-2">
+        <h2 className="text-lg font-semibold truncate">{data.title}</h2>
+        <p className="text-sm text-gray-500">
+          {format(new Date(data.releaseDate), 'LLLL do, yyyy')}
+        </p>
+      </div>
+    </li>
+  );
+};
 
 export default MoviesItem;
