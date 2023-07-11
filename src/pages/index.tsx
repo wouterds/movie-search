da41752ef942289a4eec54d50f25c4248bcdc551
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-11 13:01:43 +0200

    feat: cloudflare api cache stats

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from 'react';
+import { FaCloudflare } from 'react-icons/fa';
+import { usePrevious } from 'react-use';
 
 import Movies from '@/components/Movies';
 import { useMovieSearch, useSyncQuery } from '@/hooks';
 
 const Home = () => {
   const [q, setQ] = useState<string | null>(null);
-  const { movies } = useMovieSearch(q);
+  const { movies, isLoading, cacheHit } = useMovieSearch(q);
+  const [apiCalls, setApiCalls] = useState(0);
+  const [cacheHits, setCacheHits] = useState(0);
+
+  const wasLoading = usePrevious(isLoading);
+  useEffect(() => {
+    console.log({ isLoading, wasLoading, cacheHit });
+    if (!isLoading && wasLoading) {
+      setApiCalls(calls => calls + 1);
+      setCacheHits(hits => hits + (cacheHit ? 1 : 0));
+    }
+  }, [isLoading, wasLoading, cacheHit]);
 
   const initialQueryParam = useSyncQuery({ q });
   useEffect(() => {
@@ -15,18 +28,42 @@ const Home = () => {
   }, [initialQueryParam, q]);
 
   return (
-    <main className="flex flex-col min-h-screen w-full p-12">
-      <h1 className="text-5xl font-bold mb-6">Movie Search</h1>
-      <form className="max-w-2xl w-full mb-6">
-        <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
-          type="text"
-          placeholder="Search for any movie title.."
-          onChange={e => setQ(e.target.value)}
-          value={initialQueryParam}
-          autoFocus
-        />
-      </form>
+    <main className="flex flex-col min-h-screen w-full p-8 lg:p-12">
+      <div className="flex flex-col justify-between items-start mb-8 lg:flex-row-reverse">
+        <div className="border-2 border-orange-600 bg-gray-50 rounded-lg w-full mb-6 lg:mb-0 lg:w-48">
+          <h2 className="text-sm font-semibold uppercase bg-orange-600 text-orange-50 px-2 py-1.5 flex justify-between items-center">
+            <span>API Call Stats</span>
+            <FaCloudflare className="inline-block text-lg mr-1" />
+          </h2>
+          <div className="p-2 text-sm">
+            <p>
+              <span className="font-semibold">API calls:</span> {apiCalls}
+            </p>
+            <p>
+              <span className="font-semibold">Cloudflare CDN hits:</span>{' '}
+              {cacheHits}
+            </p>
+            <p>
+              <span className="font-semibold">Cache rate:</span>{' '}
+              {apiCalls > 0 ? Math.round((cacheHits / apiCalls) * 100) : 0}%
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full lg:max-w-2xl">
+          <h1 className="text-5xl font-bold mb-6">Movie Search</h1>
+          <form>
+            <input
+              className="block w-full p-3 text-md text-gray-900 rounded-lg bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+              type="text"
+              placeholder="Search for any movie title.."
+              onChange={e => setQ(e.target.value)}
+              value={initialQueryParam}
+              autoFocus
+            />
+          </form>
+        </div>
+      </div>
       <Movies data={movies} />
     </main>
   );
