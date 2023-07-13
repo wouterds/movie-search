3e624cbbd47f1f3709cd4f304365b380ab162ab2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-13 15:29:23 +0200

    feat: add progress bar and remove orange

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,23 +27,34 @@ const Home = () => {
 
   return (
     <main className="flex flex-col min-h-screen w-full">
-      <div className="bg-orange-600 text-white w-full text-sm px-4 sm:px-6 py-3 flex items-center">
+      <div className="bg-gray-50 text-black w-full text-sm px-4 sm:px-6 py-2.5 flex items-center">
         <ul className="inline-block">
           <li className="inline-block mx-1 sm:mx-2">
             <span className="font-semibold">API calls:</span>{' '}
-            <span className="font-medium text-white/90">{apiCalls}</span>
+            <span className="font-medium text-gray-700">{apiCalls}</span>
           </li>
           <li className="inline-block mx-1 sm:mx-2">
             <span className="font-semibold">CDN hits:</span>{' '}
-            <span className="font-medium text-white/90">{cacheHits}</span>
+            <span className="font-medium text-gray-700">{cacheHits}</span>
           </li>
           <li className="inline-block mx-1 sm:mx-2">
             <span className="font-semibold">Cache rate:</span>{' '}
-            <span className="font-medium text-white/90">
+            <span className="font-medium text-gray-700">
               {apiCalls > 0 ? Math.round((cacheHits / apiCalls) * 100) : 0}%
             </span>
           </li>
         </ul>
+      </div>
+      {/* progress bar */}
+      <div className="h-1 bg-red-500">
+        <div
+          className="h-full bg-green-500 transition-all duration-500"
+          style={{
+            width: `${
+              apiCalls > 0 ? Math.round((cacheHits / apiCalls) * 100) : 0
+            }%`,
+          }}
+        />
       </div>
 
       <div className="p-4 sm:p-6 md:p-8 lg:p-10 lg:px-12">
